2bfbe48645575de8fe09232daff995c64a09261a
Vadozed <pva.kiev@gmail.com>
2022-06-05 23:02:20 +0300

    Add var

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Print("Hello Git \n")
-	a, b, c := 6, 8, 3
-	fmt.Printf("6 + 8 + 3 = %v", a+b+c)
+	a, b, c, d := 6, 8, 3, 1
+	fmt.Printf("6 + 8 + 3 + 1 = %v", a+b+c+d)
 }
